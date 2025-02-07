96b966cc640bd1a67c4bccacb5d3aaa10b6fc226
adarsh7123 <adarshverma127@gmail.com>
2025-02-07 15:44:42 +0530

    Update deployment configurations

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://repair-service-backend.onrender.com/api'
+  : 'http://localhost:5000/api';
 
 // Create axios instance with default config
 const axiosInstance = axios.create({
